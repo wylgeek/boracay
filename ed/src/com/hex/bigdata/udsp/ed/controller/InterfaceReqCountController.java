b84b658113b08ab4cfe819832406935ff8f86a9b
tomnic.wang <tomnic.wang@goupwith.com>
2018-01-04 17:45:06 +0800

    类名修改

---
+++ ed/src/com/hex/bigdata/udsp/ed/controller/InterfaceReqCountController.java
@@ -0,0 +1,32 @@
+package com.hex.bigdata.udsp.ed.controller;
+
+import com.hex.bigdata.udsp.ed.dto.InterfaceReqCountDto;
+import com.hex.bigdata.udsp.ed.service.InterfaceReqCountService;
+import com.hex.goframe.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ed/interfaceReqCount")
+public class InterfaceReqCountController extends BaseController {
+
+    @Autowired
+    private InterfaceReqCountService interfaceReqCountService;
+
+    @RequestMapping("/getServiceCountGroupByDay")
+    @ResponseBody
+    public List<InterfaceReqCountDto> getServiceCountGroupByDay() {
+        try{
+            List<InterfaceReqCountDto> serviceCountDtos = interfaceReqCountService.getServiceCountGroupByDay();
+            return serviceCountDtos;
+        } catch (Exception e) {
+            logger.info("服务请求异常{}",this.getClass().getName());
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
